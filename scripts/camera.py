1767a01bd92d0c362fe87753352e65fd031a924d
Julian <Julian.apfelthaler@gmail.com>
2018-04-26 00:43:34 +0200

    camera.detect_color now generates a BRG histogram but refuses to update it

--- scripts/camera.py
+++ scripts/camera.py
@@ -1,6 +1,7 @@
 import rospy
 import cv2
 from cv_bridge import CvBridge,CvBridgeError
+from matplotlib import pyplot as plt
 
 class Camera:
 
@@ -8,6 +9,8 @@ class Camera:
         self.bridge = CvBridge()
         self.image_window = "Camera Input"
         self.image_data = None
+	
+	self.histograms_window = "histograms"
 
     def show(self,img_msg=None):
         """
@@ -15,7 +18,6 @@ class Camera:
             Move the time sync functions from the playnode here. The camera
             should'nt show up automatically.
         """
-        rospy.loginfo("Hello there!\n")
         try:
             image = self.bridge.imgmsg_to_cv2(img_msg, "bgr8")
         except CvBridgeError as e:
@@ -25,6 +27,7 @@ class Camera:
         self.image_data = image
         cv2.imshow(self.image_window, image)
         cv2.waitKey(10)
+	self.detect_color(self.image_data)
 
     def callback(self, img_msg=None,laser_msg=None):
         """
@@ -32,10 +35,19 @@ class Camera:
         the relevant data in an attribute.
         """
 
-    def detect_color(self,img_msg=None):
+
+    def detect_color(self,image=None):
         """
         TODO: feat_det_color, priority 4
             Extract the color from img_msg and fill the the array with it and
             return an array with the same dimensions, but with enum color
             names. Relevant is green, yellow, blue.
         """
+	color = ('b','g','r')
+	for i,col in enumerate(color):
+	  histr = cv2.calcHist([image],[i],None,[256],[0,256])
+	  plt.plot(histr,color = col)
+ 	  plt.xlim([0,256])
+	plt.ion()	#interactive mode, otherwise .show holds until window is closed
+	plt.show()
+	
