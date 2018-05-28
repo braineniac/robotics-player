b99ddd57372eb3a78a4a36427c13eff7dc7a5fec
Daniel David Hammer <daniel.david.hammer@gmail.com>
2018-05-28 23:17:56 +0200

    Created odom node, maybe works, did it kinda drunk

--- scripts/odom_node.py
+++ scripts/odom_node.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from  sensor_msgs.msg import JointState
+from team3_msgs import ScannedObjs, ScannedObji, Odom
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import TransformStamped,Quaternion
 import tf
@@ -14,76 +14,45 @@ class OdomNode:
     def __init__(self):
         rospy.init_node("odom_node")
         rosprint("Initialised odom node!")
-        self.odom_pub = rospy.Publisher("odom_node", Odometry,queue_size=1000)
-        self.broadcaster = tf2_ros.TransformBroadcaster()
+        self.scanned_sub = rospy.Subscriber("scanned_objs", ScannedObjs, self.scanned_cb)
+        self.odom_pub = rospy.Publisher("odom_node", Odom,queue_size=100)
+        self.last_time_stamp =rospy.Time.now()
+        self.last_scanned_msg
 
+    def scanned_cb(self, scanned_msg):
+        laser_time_now = rospy.Time.now()
+        laser_time_last = self.last_time_stamp
+        dt = laser_time_now - laser_time_last
+        current_peaks = []
+        last_peaks = []
+        current_ranges = []
+        for elem_current in scanned_msg.scannedObjList:
+            dist_current, angle_current = elem
+            for elem_last in self.last_scanned_msg:
+                dist_last,angle_last = elem_last
+                if angle_last -5>angle_current and angle_last+5 > angle_current:
+                    if dist_current - dist_last < 0.1:
+                        phi = angle_current - angle_last
+                        delta_x = np.cos(angle_current) * dist_current - np.cos(angle_last) * dist_last
+                        delta_y = np.sin(angle_current) * dist_current - np.cos(angle_last) * dist_last
+                        vx = delta_x / dt
+                        vy = delta_y / dt
+                        self.publish(phi, delta_x, delta_y, vx, vy)
+        self.last_scanned_msg = scanned_msg
+
+    def publish(phi, delta_x, delta_y, vx,vy):
+        odom_msg = Odom()
+        odom_msg.phi = phi
+        odom_msg.delta_x = delta_x
+        odom_msg.delta_y = delta_y
+        odom_msg.vx = vx
+        odom_msg.vy = vy
+        self.odom_pub.publish(odom_msg)
 
 if __name__ == '__main__':
 
     odom_node = OdomNode()
     loop_rate = rospy.Rate(10)
 
-    current_time = rospy.Time.now()
-    last_time = rospy.Time.now()
-    #initial position
-    x = 0.0
-    y = 0.0
-    th= 0
-
-    vx = 0
-    vy = 0
-    vth = 0
-
-    #message declarations
-    odom_trans = TransformStamped()
-    header_frame = odom_trans.header.frame_id = "odom_custom"
-    child_frame = odom_trans.child_frame_id = "base_link"
-
-
     while not rospy.is_shutdown():
-       current_time = rospy.Time.now()
-       dt = (current_time - last_time).to_sec()
-       delta_x = (vx * np.cos(th) - vy * np.sin(th)) * dt
-       delta_y = (vx * np.sin(th) + vy * np.cos(th)) * dt
-       delta_th = vth * dt
-
-       x += delta_x
-       y += delta_y
-       th += delta_th
-
-       odom_quat = Quaternion()
-       odom_quat =tf.transformations.quaternion_from_euler(0,0,th)
-
-       # update transform
-       odom_trans.header.stamp = current_time
-       odom_trans.transform.translation.x = x
-       odom_trans.transform.translation.y = y
-       odom_trans.transform.translation.z = 0.0
-       odom_trans.transform.rotation = tf.transformations.quaternion_from_euler(0,0,th)
-
-       #filling the odometry
-       odom = Odometry()
-       odom.header.stamp = current_time
-       odom.header.frame_id = "odom_custom"
-       odom.child_frame_id = "base_link"
-
-       #position
-       odom.pose.pose.position.x = x
-       odom.pose.pose.position.y = y
-       odom.pose.pose.position.z = 0.0
-       odom.pose.pose.orientation = odom_quat
-
-       #velocity
-       odom.twist.twist.linear.x = vx
-       odom.twist.twist.linear.y = vy
-       odom.twist.twist.linear.z = 0.0
-       odom.twist.twist.angular.x = 0.0
-       odom.twist.twist.angular.y = 0.0
-       odom.twist.twist.angular.z = vth
-
-       last_time = current_time
-
-       odom_node.broadcaster.sendTransform(odom_trans)
-       odom_node.odom_pub.publish(odom)
-
-       loop_rate.sleep()
+           loop_rate.sleep()
