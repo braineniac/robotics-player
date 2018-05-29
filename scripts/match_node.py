caea356126127a890a99b6e542c8d57312d00080
Julian <Julian.apfelthaler@gmail.com>
2018-05-29 19:54:34 +0200

    just removed code we were not going to use

--- scripts/match_node.py
+++ scripts/match_node.py
@@ -14,6 +14,7 @@ class MatchNode:
         self.scanned_obj_sub = rospy.Subscriber("scanned_objs", ScannedObjs,self.match)
         self.kinect_objs_sub = rospy.Subscriber("camera_objs", KinectObjs,self.camera_save)
         self.detected_obj_pub = rospy.Publisher("detected_objs", DetectedObjs,queue_size=1)
+	
 
         self.current_camera_msg = None
         rospy.spin()
@@ -31,29 +32,11 @@ class MatchNode:
         """
         TODO: make this a lot better after the fixing the laser and camera
         """
-        for camera_msg in self.current_camera_msg.kinectObjList:
-            x1 = camera_msg.lower
-            x2= camera_msg.upper
-            area = camera_msg.area
-            color = camera_msg.color
-            phi1 = 11*(x1-30)/90-55
-            phi2 = 11*(x2+30)/90-55
-            for scanned_msg in scanned_msgs.scannedObjList:
-                laser_phi = scanned_msg.angle
-                distance = scanned_msg.dist
-                if ((laser_phi>=phi1) and (laser_phi<=phi2)):
-                    if color != "G" and area>10000:
-                        if color == "Y":
-                            rosprint("Detected the yellow goal!")
-                        else:
-                            rosprint("Detected the blue goal!")
-                    elif color == "G":
-                        rospy.loginfo("Detected a pole in {} m distance and \
-                                {}degrees!".format(distance,laser_phi))
-                    elif color == "B":
-                        rospy.loginfo("Detected a blue puck in {} m distance \
-                                and {} degrees!".format(distance,laser_phi))
-
+	rospy.loginfo("=============")
+	for ray in scanned_msgs.scannedObjList:
+	    rospy.loginfo(ray.dist)
+	    rospy.loginfo(ray.angle)
+	    
 if __name__ == '__main__':
 
     match_node = MatchNode()
