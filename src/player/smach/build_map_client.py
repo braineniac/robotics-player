8a301f1c95bb4c014f356031fa4873abce5715b3
Daniel Hammer <danwald10@gmail.com>
2018-06-19 06:28:30 +0200

    Moved map building parts to smach

--- src/player/smach/build_map_client.py
+++ src/player/smach/build_map_client.py
@@ -7,20 +7,22 @@ from player.msg import *
 from tools import rosprint
 
 def done_cb(state, result):
-    rosprint("Action done: build_map, finished in state {} with result {}".format(state,result))
+#    rosprint("Action done: build_map, finished in state {} with result {}".format(state,result))
+    pass
 
 def active_cb()
-    rosprint("Action is now active: build_map")
+#    rosprint("Action is now active: build_map")
+    pass
 
 def feedback_cb(feedback):
-    rosprint("Feedback build_map from action: {}".format(feedback))
-
+#    rosprint("Feedback build_map from action: {}".format(feedback))
+    pass
 if __name__ == '__main__':
     rospy.init_node("build_map_client")
     build_map_client = SimpleActionClient("build_map", BuildMapAction)
-    rosprint("Waiting for build_map server")
+#    rosprint("Waiting for build_map server")
     build_map_client.wait_for_server()
-    rosprint("Sent data to build_map server")
+#    rosprint("Sent data to build_map server")
     build_map_goal = BuildMapGoal()
     build_map_client.send_goal(build_map_goal, done_cb=done_cb,
             active_cb=active_cb, feedback_cb=feedback_cb)
